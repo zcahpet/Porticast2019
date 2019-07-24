a74946a49c95b161e900e2eb8518d3bb07abc697
Pierre Labadie <pierr.labadie@gmail.com>
2019-07-24 11:22:21 +0100

    Added text to home_body

--- home_body.php
+++ home_body.php
@@ -6,6 +6,26 @@
 	<script type="text/javascript" src="home_script.js"></script>
 </head>
 <body>
-	<h1>HOME FOR PORTICAST</h1>
+	<h1>Welcome to Porticast</h1>
+	<div id="main">
+		<p>
+			This wensite is designed for and used by undergraduate Mechanical Engineering students at UCL. It hosts videos and tutorials for softwares such as CATIA and MatLab as well as the induction videos for the workshop at Wicklow Street. The purpose of the website is to help new students get familiar with the machines, tools and softwares they will use during their time at UCL and beyond.
+		</p>
+	</div>
+	<div id="catia">
+		<p>
+			CATIA is a CAD (Computer Aided Design) software used in many areas of engineering and throughout the industrial world by professionals to make their ideas into digital reality. This then gives an accurate estimate of the real life aesthetics and functionality of the part modelled.
+		</p>
+	</div>
+	<div id="matlab">
+		<p>
+			MatLab (Matrix Laboratory) is a programming language used for matrix manipulation, plotting graphs and many other mathematical applications. It is a useful tool for any engineer and remains relevant today, even decades after its release.
+		</p>
+	</div>
+	<div id="wicklow">
+		<p>
+			The Wicklow Street workshop opened in 2019 as a space for UCL Mechanical Engineering students to freely work on their projects and for the racing teams to manage an efficient workflow. While it is important to know how to use these machinese both for your time at UCL and what may come after, it is important to recognise the danger they pose if used poorly. These videos aim to provide adequate knowledge of the machines for them to be used safely
+		</p>
+	</div>
 </body>
 </html>
