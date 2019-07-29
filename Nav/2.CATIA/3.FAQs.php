453592a00071b1dd1f3e2e61ecf6a7a0b86adb26
Cameron Peters <cameron.peters.17@ucl.ac.uk>
2019-07-29 17:13:14 +0100

    Solved double click issue with collapsibles and slight edit to CSS

--- Nav/2.CATIA/3.FAQs.php
+++ Nav/2.CATIA/3.FAQs.php
@@ -8,56 +8,57 @@ require_once '../../headerTEST.php' ;
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="..\..\Stylesheets\catFaq_style.css">
-	<script type="text/javascript" src="..\..\Scripts\catFaq_script.js"></script>
+	<script type="text/javascript" src="..\..\Scripts\catFaq_script.js">
+	</script>
 </head>
-<body>
+<body onload="collapse()">
 	<h1>FAQs</h1>
 
 	<div id="2D">
 		<h3>2D</h3>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How can I turn off the snap to 	grid function? <br>
 		</button>
 		<div class="content">
   			<p>For the line, circle and points, they will snap to grid if the snap-to-grid icon is turned on. Unclicking it will allow them to move freely around the drawing. As for the textbox, the snap to grid option can be turned off by hitting shift. Alternatively you can disable it at option>mechanical design>drafting. Click on the “annotation and Dress-up” tab and unclick snap by default to disable it permanently.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I control size of the geometry I created? <br>
 		</button>
 		<div class="content">
   			<p>Use a dimension on the geometry, double click on the dimension line and click drive geometry to control the length/radius.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How can I move my value on the dimension line?  <br>
 		</button>
 		<div class="content">
   			<p>Go to option>Mechanical Design>Drafting and click on the Manipulator tab. Click on the modification box for move value.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			My drawing is no longer linked to my 3D model. What should I do? <br>
 		</button>
 		<div class="content">
   			<p>Go to Files>Desk. On this view, right click on the broken link and then click find to locate your 3D CATPart</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why can’t I edit a view that I want to work on? <br>
 		</button>
 		<div class="content">
   			<p>The view that you would like to work on is not selected; make sure the view frame is double clicked.  The frame will be red if it is selected.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why do my lines, points and circles move away with another view that it is not associated with? <br>
 		</button>
 		<div class="content">
   			<p>The lines, points and circles are created in another frame. View is selected before any profile is created.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why is my dimension line coloured? <br>
 		</button>
 		<div class="content">
   			<p>The dimension line is coloured due to issues on the drawing or 3D model. You check on Tools>Options>Mechanical Design>Drafting, choose the Dimension tab and scroll all the way down to click the TYPES AND COLORS button. This will tell you what all the colors mean.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why can’t my line, point or circle snapped onto my drawings?
 		</button>
 		<div class="content">
@@ -66,55 +67,55 @@ require_once '../../headerTEST.php' ;
 	</div>
 	<div id="3D">
 		<h3>3D</h3>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Can I use other file types in CATIA? <br>
 		</button>
 		<div class="content">
   			<p>Yes, there a few other file types that CATIA can open. These include .stl .stp .step .dxf . You also have to make sure that you have the right permissions to use these files as you may find that without a correct license the file will not appear.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why is the tree moving with my mouse instead of the part? <br>
 		</button>
 		<div class="content">
   			<p>You will see this issue immediately, since the part will fade to grey. This happens because you may have clicked on the white stem of the tree by accident. So CATIA thinks that you are trying to adjust the tree rather than the part. To fix this, click on the white part of the tree to restore part control.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">			Can I download parts off the internet? <br>
+		<button class="collapsible" >			Can I download parts off the internet? <br>
 		</button>
 		<div class="content">
   			<p>There are many sites that allow you to download 3D models from their database. Most of the time you will have to sign up in order to do so, but they are usually free services. Communal database sites such as GrabCAD are very useful for experimenting, but it is also worth noting that manufacturer websites such as SKF provide a catalogue from which you can download models to use in CATIA. This can be very useful when creating assemblies in which you need a bearing etc.<br>
 			It goes without saying, make sure you credit any downloaded models in your work!</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Can I render in CATIA? <br>
 		</button>
 		<div class="content">
   			<p>Yes, rendering can be done in CATIA. There is a tool which resembles a camera which can be used to quickly render any part or assembly that you are working on. In order to achieve the best results you may wish to apply a material to your part.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Can I use CATIA without a mouse? <br>
 		</button>
 		<div class="content">
   			<p>Yes, you can still drive the software without a mouse but it is much more efficient to use one. It will improve your workflow tremendously and so it is well worth investing in a mouse.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Can I add to an existing part? <br>
 		</button>
 		<div class="content">
   			<p>You can add to a sketch which you have previously created without too much trouble; simply go to the sketch in the tree and double click it. As long as your addition does not affect other aspects of the part, you should be ok. If there are other sketches reliant on the sketch you wish to alter, be sure to alter their constraints appropriately.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I reset the compass? <br>
 		</button>
 		<div class="content">
   			<p>This is an easy fix; select the view tab and then select reset compass. Simple!</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How can I see my sketches? <br>
 		</button>
 		<div class="content">
   			<p>There is a single button that can quickly toggle between your part and the sketches that you made in the creation of that part. It can be found in the view toolbar and is labelled as “swap visible space”.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I apply a material to my part? <br>
 		</button>
 		<div class="content">
@@ -122,7 +123,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I snap the compass to an object? <br>
 		</button>
 		<div class="content">
@@ -130,13 +131,13 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I import parts into an assembly? <br>
 		</button>
 		<div class="content">
   			<p>There are two ways to do this. If you already have the part open in a separate window you can drag the part from its tree on to the assembly tree. Alternatively go to the insert tab and then click “insert part”.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I retrieve parts from an assembly? <br>
 		</button>
 		<div class="content">
@@ -144,7 +145,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I check the mass my part/assembly? <br>
 		</button>
 		<div class="content">
@@ -152,7 +153,7 @@ require_once '../../headerTEST.php' ;
 				<img src="p.png">
 			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I show parameters and values? <br>
 		</button>
 		<div class="content">
@@ -160,7 +161,7 @@ require_once '../../headerTEST.php' ;
 				<img src="p.png">
 			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			How do I turn the snap grid on and off in CATIA sketch mode? <br>
 		</button>
 		<div class="content">
@@ -168,7 +169,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			My part tree has disappeared? <br>
 		</button>
 		<div class="content">
@@ -176,7 +177,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			My part has been greyed out? <br>
 		</button>
 		<div class="content">
@@ -184,13 +185,13 @@ require_once '../../headerTEST.php' ;
 				<img src="p.png">
 			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Should I use a pocket or a hole? <br>
 		</button>
 		<div class="content">
   			<p>It depends on your application. Visually they will both yield the same result, but a hole will mostly be used when you wish to add a thread. If your purpose is to simply remove material, then a pocket should be used.</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Where can I find my toolbars? <br>
 		</button>
 		<div class="content">
@@ -198,7 +199,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Where has my axis system gone? <br>
 		</button>
 		<div class="content">
@@ -206,7 +207,7 @@ require_once '../../headerTEST.php' ;
   				<img src="p.png">
   			</p>
 		</div>
-		<button class="collapsible" onclick="collapse()">
+		<button class="collapsible" >
 			Why is my constraint purple? <br>
 		</button>
 		<div class="content">
