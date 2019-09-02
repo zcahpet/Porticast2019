68d4b2ecec9dc6009a980135de6ea9ab60871dda
Dan Thompson <zcemdjt@ucl.ac.uk>
2019-09-02 13:38:10 +0100

    Footer links

--- footer.php
+++ footer.php
@@ -9,10 +9,10 @@
 <footer>
 		<nav>
 			<ul>
-				<li><a href="index.html"><b>HOME</b></a></li>
-				<li><a href="index.html"><b>MOODLE</b></a></li>
-				<li><a href="index.html"><b>SUPPORT</b></a></li>
-				<li><a href="index.html"><b>ABOUT</b></a></li>
+				<li><a href="https://www.ucl.ac.uk/"><b>HOME</b></a></li>
+				<li><a href="https://moodle.ucl.ac.uk/"><b>MOODLE</b></a></li>
+				<li><a href="https://www.ucl.ac.uk/isd/help-support"><b>SUPPORT</b></a></li>
+				<li><a href="about.html"><b>ABOUT</b></a></li>
 			</ul>
 		</nav>	
 	</footer>
