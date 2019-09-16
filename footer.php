309f98f686aff719a2d95aead5649c166214010b
Dan Thompson <zcemdjt@ucl.ac.uk>
2019-09-16 16:50:04 +0100

    Brand Colour
    Black and orange update

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 				<li><a href="https://www.ucl.ac.uk/"><b>HOME</b></a></li>
 				<li><a href="https://moodle.ucl.ac.uk/"><b>MOODLE</b></a></li>
 				<li><a href="https://www.ucl.ac.uk/isd/help-support"><b>SUPPORT</b></a></li>
-				<li><a href="about.html"><b>ABOUT</b></a></li>
+				<li><a href=""><b>ABOUT</b></a></li>
 			</ul>
 		</nav>	
 	</footer>
