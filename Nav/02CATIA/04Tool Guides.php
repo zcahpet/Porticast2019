0ae4a3cb4304e6d0d5369f9de5a61cd545763884
Pierre Labadie <pierr.labadie@gmail.com>
2019-09-23 10:23:29 +0100

    Tool Guides page added

---
+++ Nav/02CATIA/04Tool Guides.php
@@ -0,0 +1,202 @@
+<?php
+$title= "Catia Tool Guides";
+?>
+
+<html>
+ 
+<head>
+ 	<link rel="stylesheet" type="text/css" href="Stylesheets\tools.css">
+</head>
+ 
+<body>
+	<div id="background">
+		<article>
+			<ol class='nav_alphabet'>
+				<li class='alphabet_item'><a href='#A'>A</a></li>
+				<li class='alphabet_item'><a href='#B'>B</a></li>
+				<li class='alphabet_item'><a href='#C'>C</a></li>
+				<li class='alphabet_item'><a href='#D'>D</a></li>
+				<li class='alphabet_item'><a href='#E'>E</a></li>
+				<li class='alphabet_item'><a href='#F'>F</a></li>
+				<li class='alphabet_item'><a href='#G'>G</a></li>
+				<li class='alphabet_item'><a href='#H'>H</a></li>
+				<li class='alphabet_item'><a href='#I'>I</a></li>
+				<li class='alphabet_item'><a href='#J'>J</a></li>
+				<li class='alphabet_item'><a href='#K'>K</a></li>
+				<li class='alphabet_item'><a href='#L'>L</a></li>
+				<li class='alphabet_item'><a href='#M'>M</a></li>
+				<li class='alphabet_item'><a href='#N'>N</a></li>
+				<li class='alphabet_item'><a href='#O'>O</a></li>
+				<li class='alphabet_item'><a href='#P'>P</a></li>
+				<li class='alphabet_item'><a href='#Q'>Q</a></li>
+				<li class='alphabet_item'><a href='#R'>R</a></li>
+				<li class='alphabet_item'><a href='#S'>S</a></li>
+				<li class='alphabet_item'><a href='#T'>T</a></li>
+				<li class='alphabet_item'><a href='#U'>U</a></li>
+				<li class='alphabet_item'><a href='#V'>V</a></li>
+				<li class='alphabet_item'><a href='#W'>W</a></li>
+				<li class='alphabet_item'><a href='#X'>X</a></li>
+				<li class='alphabet_item'><a href='#Y'>Y</a></li>
+				<li class='alphabet_item'><a href='#Z'>Z</a></li>
+			</ol>
+			<div id='A'>
+				<h1>A</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='B'>
+				<h1>B</h1>
+				Beach <br>
+				Bright <br>
+				Blank <br>
+			</div>
+			<div id='C'>
+				<h1>C</h1>
+				Crow <br>
+				Caterpillar <br>
+				Cop <br>
+			</div>
+			<div id='D'>
+				<h1>D</h1>
+				Deign <br>
+				Drag <br>
+				Definite <br>
+			</div>
+			<div id='E'>
+				<h1>E</h1>
+				Epsom <br>
+				Elephant <br>
+				Excuse <br>
+			</div>
+			<div id='F'>
+				<h1>F</h1>
+				Fruit <br>
+				Flower <br>
+				Fountain <br>
+			</div>
+			<div id='G'>
+				<h1>G</h1>
+				Golf <br>
+				Guitar <br>
+				Gap <br>
+			</div>
+			<div id='H'>
+				<h1>H</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='I'>
+				<h1>I</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='J'>
+				<h1>J</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='K'>
+				<h1>K</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='L'>
+				<h1>L</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='M'>
+				<h1>M</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='N'>
+				<h1>N</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='O'>
+				<h1>O</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='P'>
+				<h1>P</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='Q'>
+				<h1>Q</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='R'>
+				<h1>R</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='S'>
+				<h1>S</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='T'>
+				<h1>T</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='U'>
+				<h1>U</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='V'>
+				<h1>V</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='W'>
+				<h1>W</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='X'>
+				<h1>X</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='Y'>
+				<h1>Y</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+			<div id='Z'>
+				<h1>Z</h1>
+				Alpha <br>
+				Apricot <br>
+				After <br>
+			</div>
+  		</article>
+	</div>
+</body>
+ 
+</html>
