b1bdc3f962eeb3a203652ca0ed87c909f5010cf8
Pierre Labadie <pierr.labadie@gmail.com>
2019-09-23 12:03:51 +0100

    Tools Guides page edited and Tools folder created

--- Nav/02CATIA/04Tool Guides.php
+++ Nav/02CATIA/04Tool Guides.php
@@ -13,187 +13,108 @@ $title= "Catia Tool Guides";
 		<article>
 			<ol class='nav_alphabet'>
 				<li class='alphabet_item'><a href='#A'>A</a></li>
-				<li class='alphabet_item'><a href='#B'>B</a></li>
+				<li class='alphabet_inactive'><a href='#B'>B</a></li>
 				<li class='alphabet_item'><a href='#C'>C</a></li>
 				<li class='alphabet_item'><a href='#D'>D</a></li>
-				<li class='alphabet_item'><a href='#E'>E</a></li>
+				<li class='alphabet_inactive'><a href='#E'>E</a></li>
 				<li class='alphabet_item'><a href='#F'>F</a></li>
 				<li class='alphabet_item'><a href='#G'>G</a></li>
 				<li class='alphabet_item'><a href='#H'>H</a></li>
 				<li class='alphabet_item'><a href='#I'>I</a></li>
-				<li class='alphabet_item'><a href='#J'>J</a></li>
-				<li class='alphabet_item'><a href='#K'>K</a></li>
+				<li class='alphabet_inactive'><a href='#J'>J</a></li>
+				<li class='alphabet_inactive'><a href='#K'>K</a></li>
 				<li class='alphabet_item'><a href='#L'>L</a></li>
 				<li class='alphabet_item'><a href='#M'>M</a></li>
-				<li class='alphabet_item'><a href='#N'>N</a></li>
-				<li class='alphabet_item'><a href='#O'>O</a></li>
+				<li class='alphabet_inactive'><a href='#N'>N</a></li>
+				<li class='alphabet_inactive'><a href='#O'>O</a></li>
 				<li class='alphabet_item'><a href='#P'>P</a></li>
-				<li class='alphabet_item'><a href='#Q'>Q</a></li>
+				<li class='alphabet_inactive'><a href='#Q'>Q</a></li>
 				<li class='alphabet_item'><a href='#R'>R</a></li>
 				<li class='alphabet_item'><a href='#S'>S</a></li>
 				<li class='alphabet_item'><a href='#T'>T</a></li>
 				<li class='alphabet_item'><a href='#U'>U</a></li>
-				<li class='alphabet_item'><a href='#V'>V</a></li>
+				<li class='alphabet_inactive'><a href='#V'>V</a></li>
 				<li class='alphabet_item'><a href='#W'>W</a></li>
-				<li class='alphabet_item'><a href='#X'>X</a></li>
-				<li class='alphabet_item'><a href='#Y'>Y</a></li>
-				<li class='alphabet_item'><a href='#Z'>Z</a></li>
+				<li class='alphabet_inactive'><a href='#X'>X</a></li>
+				<li class='alphabet_inactive'><a href='#Y'>Y</a></li>
+				<li class='alphabet_inactive'><a href='#Z'>Z</a></li>
 			</ol>
-			<div id='A'>
+			<div class='alphabet_index' id='A'>
 				<h1>A</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Add'>Add</a><br>
 			</div>
-			<div id='B'>
-				<h1>B</h1>
-				Beach <br>
-				Bright <br>
-				Blank <br>
-			</div>
-			<div id='C'>
+			<div class='alphabet_index' id='C'>
 				<h1>C</h1>
-				Crow <br>
-				Caterpillar <br>
-				Cop <br>
+				<a href='Tools/Catalogue.php'>Catalogue Browser</a> <br>
+				<a href='Tools/Chamfer'>Chamfer</a><br>
+				<a href='Tools/Constraints'>Constraints Defined in Dialogue Box</a><br>
+				<a href='Tools/Circular'>Circular Pattern</a><br>
+				<a href='Tools/Close'>Close Surface</a><br>
 			</div>
-			<div id='D'>
+			<div class='alphabet_index' id='D'>
 				<h1>D</h1>
-				Deign <br>
-				Drag <br>
-				Definite <br>
+				<a href='Tools/Dynamic'>Dynamic Sectioning</a><br>
 			</div>
-			<div id='E'>
-				<h1>E</h1>
-				Epsom <br>
-				Elephant <br>
-				Excuse <br>
-			</div>
-			<div id='F'>
+			<div class='alphabet_index' id='F'>
 				<h1>F</h1>
-				Fruit <br>
-				Flower <br>
-				Fountain <br>
+				<a href='Tools/Fillet'>Fillet Radius</a><br>
 			</div>
-			<div id='G'>
+			<div class='alphabet_index' id='G'>
 				<h1>G</h1>
-				Golf <br>
-				Guitar <br>
-				Gap <br>
+				<a href='Tools/Groove'>Groove</a><br>
 			</div>
-			<div id='H'>
+			<div class='alphabet_index' id='H'>
 				<h1>H</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Hole'>Hole</a><br>
 			</div>
-			<div id='I'>
+			<div class='alphabet_index' id='I'>
 				<h1>I</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Intersect'>Intersect</a><br>
 			</div>
-			<div id='J'>
-				<h1>J</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='K'>
-				<h1>K</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='L'>
+			<div class='alphabet_index' id='L'>
 				<h1>L</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Line'>Line</a><br>
 			</div>
-			<div id='M'>
+			<div class='alphabet_index' id='M'>
 				<h1>M</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Mirror'>Mirror</a><br>
+				<a href='Tools/MultiSection'>Multi-Section Solid</a><br>
 			</div>
-			<div id='N'>
-				<h1>N</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='O'>
-				<h1>O</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='P'>
+			<div class='alphabet_index' id='P'>
 				<h1>P</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Pad'>Pad</a><br>
+				<a href='Tools/Plane'>Plane</a><br>
+				<a href='Tools/Pocket'>Pocket</a><br>
+				<a href='Tools/Points'>Points</a><br>
 			</div>
-			<div id='Q'>
-				<h1>Q</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='R'>
+			<div class='alphabet_index' id='R'>
 				<h1>R</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Remove'>Remove</a><br>
+				<a href='Tools/Rectangular'>Rectangular Pattern</a><br>
 			</div>
-			<div id='S'>
+			<div class='alphabet_index' id='S'>
 				<h1>S</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Scaling'>Scaling</a><br>
+				<a href='Tools/Shaft'>Shaft</a><br>
+				<a href='Tools/Shell'>Shell</a><br>
+				<a href='Tools/Sketch'>Sketch Solving Status</a><br>
+				<a href='Tools/Split'>Split</a><br>
 			</div>
-			<div id='T'>
+			<div class='alphabet_index' id='T'>
 				<h1>T</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Thickness'>Thickness</a><br>
+				<a href='Tools/Tick_Profile'>Thick Profile</a><br>
+				<a href='Tools/Thick_Surface'>Thick Surface</a><br>
+				<a href='Tools/Translation'>Translation</a><br>
+				<a href='Tools/Trim'>Trim</a><br>
 			</div>
-			<div id='U'>
+			<div class='alphabet_index' id='U'>
 				<h1>U</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/User'>User Pattern</a><br>
 			</div>
-			<div id='V'>
-				<h1>V</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='W'>
+			<div class='alphabet_index' id='W'>
 				<h1>W</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='X'>
-				<h1>X</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='Y'>
-				<h1>Y</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
-			</div>
-			<div id='Z'>
-				<h1>Z</h1>
-				Alpha <br>
-				Apricot <br>
-				After <br>
+				<a href='Tools/Wall'>Wall Thickness Analysis</a><br>
 			</div>
   		</article>
 	</div>
