30ba8b0f53155053a4bb1b6802f8519dd531d9f4
Dan Thompson <zcemdjt@ucl.ac.uk>
2019-09-24 15:43:00 +0100

    Video Details

--- Nav/05Wicklow/03Laser Cutter & 3D Printer.php
+++ Nav/05Wicklow/03Laser Cutter & 3D Printer.php
@@ -5,10 +5,10 @@
 
 <?php 
 $data = [
-['video_title' =>'3D Printer: How to print','id'=>'hyiPyaR_pv0', 'description'=>"Welcome to this video series! This tutorial has very basic information about CATIA's interface and how to export ready for printing."],
-['video_title' =>'3D Printer - 2','id'=>'', 'description'=>'Welcome to this video series! This video will introduce you to creating a CATPart file and the basic elements that you can use in a sketch such as shapes and constraints.'],
-['video_title' =>'3D Printer - 3','id'=>'', 'description'=>'Welcome to this video series! This tutorial teaches you how to create a pad from an existing sketch and how to rename parts.'],
-['video_title' =>'Using the Laser Cutter','id'=>'QUHAxgWJdLw', 'description'=>'Welcome to this induction video on how to safely use the laser cutter'],
+['video_title' =>'3D Printer: How to print','id'=>'hyiPyaR_pv0', 'description'=>"Welcome to this video series! This tutorial has very basic information about the print studio interface and how to export from CATIA ready for printing."],
+['video_title' =>'3D Printer: Parameters','id'=>'t3Ui77G4yBQ', 'description'=>'Welcome to this video series!'],
+['video_title' =>'3D Printer: Paramters continued','id'=>'', 'description'=>'Welcome to this video series!'],
+['video_title' =>'Using the Laser Cutter','id'=>'QUHAxgWJdLw', 'description'=>'Welcome to this induction video on how to safely use the Laser cutter.'],
 ];
 create_video_list($data);
 
