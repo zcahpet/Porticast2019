654fd3c88e796fe1d9a16e04ba344409ee155869
Dan Thompson <zcemdjt@ucl.ac.uk>
2019-09-24 15:13:54 +0100

    Video Rename

--- Nav/05Wicklow/02Workshop Tools.php
+++ Nav/05Wicklow/02Workshop Tools.php
@@ -5,12 +5,12 @@
 
 <?php
 $data = [
-['video_title' =>"PPE Do's and Don'ts",'id'=>'2QiNIust0cg', 'description'=>'Welcome to this induction video personal protective equipment'],
-['video_title' =>'Cordless Drill','id'=>'HYWfs7x_p04', 'description'=>'Welcome to this induction video on how to safely use the cordless drill'],
-['video_title' =>'Glue Gun','id'=>'qvpihF6CpuE', 'description'=>'Welcome to this induction video on how to safely use the glue gun'],
-['video_title' =>'Soldering Iron','id'=>'Ip-GeDaAfQk', 'description'=>'Welcome to this induction video on how to safely use the soldering iron'],
-['video_title' =>'Pillar Drill','id'=>'bPAyz0H8maw', 'description'=>'Welcome to this induction video on how to safely use the pillar drill'],
-['video_title' =>'Dremel','id'=>'', 'description'=>'Welcome to this induction video on how to safely use the dremel'],
+['video_title' =>"PPE Do's and Don'ts",'id'=>'2QiNIust0cg', 'description'=>'Welcome to this induction video personal protective equipment.'],
+['video_title' =>'Cordless Drill','id'=>'HYWfs7x_p04', 'description'=>'Welcome to this induction video on how to safely use the cordless drill.'],
+['video_title' =>'Glue Gun','id'=>'qvpihF6CpuE', 'description'=>'Welcome to this induction video on how to safely use the glue gun.'],
+['video_title' =>'Soldering Iron','id'=>'Ip-GeDaAfQk', 'description'=>'Welcome to this induction video on how to safely use the soldering iron.'],
+['video_title' =>'Pillar Drill','id'=>'bPAyz0H8maw', 'description'=>'Welcome to this induction video on how to safely use the pillar drill.'],
+['video_title' =>'Dremel','id'=>'Gsx7Rk13GY4', 'description'=>'Welcome to this induction video on how to safely use the dremel.'],
 ];
 create_video_list($data);
 
