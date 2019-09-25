72534265f59ca3083c2e63746e01ca05d74697a6
Pierre Labadie <pierr.labadie@gmail.com>
2019-09-25 10:24:00 +0100

    Constraints Definition Tool done (+ Padding added to pictures)

--- Tools/Chamfer.php
+++ Tools/Chamfer.php
@@ -9,19 +9,19 @@
 				<br>
 				Click on the Chamfer tool.
 				<br>
-				<img src="Tools/Tools_Images/Chamfer/chamfer_icon.gif">
+				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Chamfer/chamfer_icon.gif">
 				<br>
 				Typically, you will want to create a Chamfer using a length and angle definition. Your CATIA may have the “length and length” setting selected as default, which can be changed by entering the drop down menu and selecting the “length and angle” option as shown below.
 				<br>
-				<img src="Tools/Tools_Images/Chamfer/chamfer_menu.png">
+				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Chamfer/chamfer_menu.png">
 				<br>
 				Click the chamfer tool, and then select an edge to chamfer. Choose the desired parameters. A representation of your selection should be shown in green on the edge that you have selected.
 				<br>
-				<img src="Tools/Tools_Images/Chamfer/chamfer_preview.png">
+				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Chamfer/chamfer_preview.png">
 				<br>
 				Click OK, and the part should look like this.
 				<br>
-				<img src="Tools/Tools_Images/Chamfer/chamfer_result.png">
+				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Chamfer/chamfer_result.png">
 			</p>
 	</article>
 	</div>
