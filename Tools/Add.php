9acb9c25a38388a0e6a51a03b8d72fed68197b63
Pierre Labadie <pierr.labadie@gmail.com>
2019-09-26 13:55:11 +0100

    create class and add tools image to CSS

--- Tools/Add.php
+++ Tools/Add.php
@@ -9,15 +9,15 @@
 				<br>
 				Click on the Add tool.
 				<br>
-				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Add/add_icon.png">
+				<img class="tools_image" src="Tools/Tools_Images/Add/add_icon.png">
 				<br>
 				A pop up box will appear. Select the secondary body as the 'Add' part and the primary body as the 'To' part.
 				<br>
-				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Add/add_parts.png">
+				<img class="tools_image" src="Tools/Tools_Images/Add/add_parts.png">
 				<br>
 				Click OK, the intersecting objects should merge. You will notice that the bodies have merged into 1 Part Body in the tree, and “Add” appears on a branch of the tree.
 				<br>
-				<img style="padding: 10px 0px;" src="Tools/Tools_Images/Add/add_product.png">
+				<img class="tools_image" src="Tools/Tools_Images/Add/add_product.png">
 			</p>
 	</article>
 	</div>
